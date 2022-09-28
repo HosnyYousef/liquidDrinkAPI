5f8825c1d472ee004414712bf8c93fae2e64234e
Yousef Hosny <yousefhosny.hm@gmail.com>
2022-09-28 17:24:20 +0200

    added different names for html titles, and copied code to memorize

--- js/main.js
+++ js/main.js
@@ -12,53 +12,55 @@
 //https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
 
 
+
 //----------------------------
 
-document.querySelector('button').addEventListener('click', getDrink)
-function getDrink(){
-    let drink = document.querySelector('input').value
+document.getElementById("getButton").addEventListener("click", getDrink)
+document.getElementById("stopButton").addEventListener("click", stop)
 
+let drinkArray = []
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}&filter.php?a=Non_Alcoholic`)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
+function getDrink() {
 
-        
-      console.log(data.drinks[0])
-      document.querySelector('h2').innerText = data.drinks[0].strDrink
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
-      document.querySelector('h3').innerText = data.drinks[0].idDrink
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+  let drink = document.querySelector("input").value
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}&filter.php?a=Non_Alcoholic`) // Make request to server.
+  .then(res => res.json()) // parse response as JSON // Treat what's coming back as JSON.
+  .then(data => { // Pass whatever we get back into "data".
+    for(let x = 0; x < data.drinks.length; x++) {
+      drinkArray.push(data.drinks[x])
+    }
+
+    console.log(drinkArray)
+
+    document.querySelector("h2").innerText = drinkArray[0].strDrink
+    document.querySelector("img").src = drinkArray[0].strDrinkThumb
+    document.querySelector("h3").innerText = drinkArray[0].strInstructions
+  })
+
+  .catch(err => { // If something does wrong, we'll get an error printed to the console.
+    console.log(`error ${err}`)
+  });
+
+  start()
 }
 
+function showDrink() {
+  document.querySelector("h2").innerText = drinkArray[i].strDrink
+  document.querySelector("img").src = drinkArray[i].strDrinkThumb
+  document.querySelector("h3").innerText = drinkArray[i].strInstructions 
+  i++
+}
 
+let intervalID
 
+let i = 1
 
-//----------------------------
+function start() {
+  intervalID = setInterval(showDrink, 5000);
+}
 
-// document.querySelector('button').addEventListener('click', getDrink)
-// function getDrink(){
-//     let drink = document.querySelector('input').value
-//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${drink}`)
-//     .then(res => res.json()) // parse response as JSON
-//     .then(data => {
-//       console.log(data.drinks)
-//       document.querySelector('h2').innerText = data.drinks[0].strDrink
-//       document.querySelector('img').src = data.drinks[0].strDrinkThumb
-//       document.querySelector('h3').innerText = data.drinks[0].strInstructions
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     });
-// }
+function stop() {
+  clearInterval(intervalID);
+}
 
-//----------------------------
-
-
-// www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic
-
-
-//----------------------------
